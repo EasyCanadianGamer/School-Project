efe2033ea4c7c7c108d5819f936c58c89bed5de2
EasyCanadianGamer <39717944+EasyCanadianGamer@users.noreply.github.com>
2021-05-24 12:46:10 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 $( document ).ready(function() {
 	$("#welcome").hide();
+  $(".upload-group").hide();
+	document.getElementById("upload").addEventListener('change', handleFileSelect, false);
 });
 
 
@@ -9,28 +11,28 @@ function signIn(){
 
 var provider = new firebase.auth.GoogleAuthProvider();
 
- firebase.auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    /** @type {firebase.auth.OAuthCredential} */
-    var credential = result.credential;
+firebase.auth()
+.signInWithPopup(provider)
+.then((result) => {
+  /** @type {firebase.auth.OAuthCredential} */
+  var credential = result.credential;
 
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    showWelcomeContainer();
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  showWelcomeContainer();
+  // ...
+}).catch((error) => {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
 
   console.log("signed in!")
 }
@@ -50,7 +52,42 @@ function showWelcomeContainer() {
 
 
 
-firebase.database().ref('Users/' + user.uid).set({
-  name: user.displayName,
-  email: user.email
-  });
+
+
+
+
+
+function handleFileSelect(event) {
+	$(".upload-group").show();
+	selectedFile = event.target.files[0];
+};
+
+function confirmUpload() {
+	var metadata = {
+		contentType: 'image',
+		customMetadata: {
+			'dogType': 'Lab',
+			'uploadedBy': user.uid,
+			'title': $("#imgTitle").val(),
+			'caption': $("#imgDesc").val()
+		},
+	};
+	var uploadTask = firebase.storage().ref().child('schoolfiles/' + selectedFile.name).put(selectedFile, metadata);
+	// Register three observers:
+	// 1. 'state_changed' observer, called any time the state changes
+	// 2. Error observer, called on failure
+	// 3. Completion observer, called on successful completion
+	uploadTask.on('state_changed', function(snapshot){
+  		// Observe state change events such as progress, pause, and resume
+  		// See below for more detail
+	}, function(error) {
+  		// Handle unsuccessful uploads
+	}, function() {
+  		// Handle successful uploads on complete
+  		// For instance, get the download URL: https://firebasestorage.googleapis.com/...
+  		$(".upload-group")[0].before("Success!");
+  		$(".upload-group").hide();
+
+	});
+
+}
